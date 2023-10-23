143365b01725bfbd960680417992270f832846e9
KOSASIH <kosasihg88@gmail.com>
2023-10-23 14:43:48 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+{
+  "sources": [
+    {
+      "name": "telescope",
+      "api_key": "YOUR_TELESCOPE_API_KEY",
+      "url": "https://api.telescope.com/data"
+    },
+    {
+      "name": "satellite",
+      "api_key": "YOUR_SATELLITE_API_KEY",
+      "url": "https://api.satellite.com/data"
+    },
+    {
+      "name": "database",
+      "api_key": "YOUR_DATABASE_API_KEY",
+      "url": "https://api.database.com/data"
+    }
+  ],
+  "output_file": "cosmic_data.csv"
+}
